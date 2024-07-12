ede59377959880e2f9ca0eca4310ce64f8d606c7
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-12 17:25:31 +0200

    Minify script.js

--- src/script.js
+++ src/script.js
@@ -1,44 +1,29 @@
-//@ts-check
-
-const titles = {
-  header: { selector: "header-page-top", title: "Accessibility guidelines" },
-  webInterface: { selector: "web-interface", title: "Web Interface" },
-  interactivity: { selector: "interactivity", title: "Interactivity" },
-  typography: { selector: "typography", title: "Typography" },
-  motion: { selector: "motion", title: "Motion" },
-  touch: { selector: "touch", title: "Touch" },
-  optimization: { selector: "optimization", title: "Optimization" },
-  accessibility: { selector: "accessibility", title: "Accessibility" },
-  design: { selector: "design", title: "Design" },
-  footnotes: { selector: "footnotes", title: "Footnotes" },
-};
-
-const baseStr = "Guidelines";
-const documentTitle = document.querySelector("#document-title");
-if (!documentTitle) {
-  throw new Error("Document title element not found");
-}
-
-const handleIntersection = (entries, options) => {
-  const { onIntersect, onDisappear } = options;
-  entries.forEach((entry) => (entry.isIntersecting ? onIntersect() : onDisappear()));
-};
-
-const voidCb = () => {};
-const setupIntersectionObserver = (element, { threshold = 0.9, onIntersect = voidCb, onDisappear = voidCb }) => {
-  const options = { onIntersect, onDisappear };
-  const observer = new IntersectionObserver((entry) => handleIntersection(entry, options), {
-    threshold: threshold,
-  });
-
-  observer.observe(element);
-};
-
-Object.values(titles).forEach((title) => {
-  setupIntersectionObserver(document.querySelector(`#${title.selector}`), {
-    onIntersect: () =>
-      (documentTitle.textContent =
-        // If the user is at the top of the page, show the default title
-        title.selector === "header-page-top" ? title.title : `${baseStr} -/- ${title.title}`),
+const ts = {
+    header: { selector: "header-page-top", title: "Accessibility guidelines" },
+    webInterface: { selector: "web-interface", title: "Web Interface" },
+    interactivity: { selector: "interactivity", title: "Interactivity" },
+    typography: { selector: "typography", title: "Typography" },
+    motion: { selector: "motion", title: "Motion" },
+    touch: { selector: "touch", title: "Touch" },
+    optimization: { selector: "optimization", title: "Optimization" },
+    accessibility: { selector: "accessibility", title: "Accessibility" },
+    design: { selector: "design", title: "Design" },
+    footnotes: { selector: "footnotes", title: "Footnotes" },
+  },
+  dt = document.querySelector("#document-title");
+if (!dt) throw Error("Document title element not found");
+const handleIntersection = (e, t) => {
+    let { onIntersect: i, onDisappear: o } = t;
+    e.forEach((e) => (e.isIntersecting ? i() : o()));
+  },
+  voidCb = () => {},
+  setupIntersectionObserver = (e, { threshold: t = 0.9, onIntersect: i = voidCb, onDisappear: o = voidCb }) => {
+    let n = { onIntersect: i, onDisappear: o },
+      s = new IntersectionObserver((e) => handleIntersection(e, n), { threshold: t });
+    s.observe(e);
+  };
+Object.values(ts).forEach((e) => {
+  setupIntersectionObserver(document.querySelector(`#${e.selector}`), {
+    onIntersect: () => (dt.textContent = "header-page-top" === e.selector ? e.title : `Guidelines -/- ${e.title}`),
   });
 });
